b2cc19e9c43a7690cb92f2e24f1d2ff75d4baa9f
kdobrovolsky <kdobrovolsky02@gmail.com>
2025-11-11 20:54:38 +0300

    first commit

---
+++ src/main.tsx
@@ -0,0 +1,11 @@
+import {createRoot} from 'react-dom/client'
+import './index.css'
+
+import {App} from "@/App/ui/App/App.tsx";
+import {BrowserRouter} from "react-router";
+
+createRoot(document.getElementById('root')!).render(
+  <BrowserRouter>
+    <App/>
+  </BrowserRouter>,
+)
